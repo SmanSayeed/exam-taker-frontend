f72aea7cdb6409d73f3dc241ef26a3dc80d20658
saadman.sayeed <saadman.sayeed@bjitgroup.com>
2024-08-08 17:43:38 +0600

    fresh repo

--- src/dashboard/components/atoms/PageTitle.jsx
+++
@@ -1,6 +0,0 @@
-const PageTitle = ({title}) => {
-  return (
-    <h1 className="text-xl font-medium" > {title} page</h1>
-  )
-}
-export default PageTitle
--- src/hooks/useAdminAuth.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-export default function useAdminAuth() {
-    const auth = useSelector(state => state.auth);
-    const isAdmin = auth?.admin_user?.roles.some(role => role.toLowerCase() === "admin");
-
-    return isAdmin;
-}
