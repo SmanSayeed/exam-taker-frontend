c8726b5759776b1c9be847acdb0f9f1cc571e6d5
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2024-08-27 20:21:09 +0600

    fixed conflict

--- src/dashboard/components/atoms/CustomSelect.jsx
+++ src/dashboard/components/atoms/CustomSelect.jsx
@@ -32,36 +32,4 @@ const CustomSelect = ({ label, categoryData, control }) => {
     )
 }
 
-export default CustomSelectimport { Label } from "@/components/ui/label";
-
-const CustomSelect = ({ label, categoryData, control }) => {
-
-    return (
-        <div className="grid gap-2">
-            <Label>{label}</Label>
-            <Controller
-                name={label}
-                control={control}
-                rules={{ required: `${label} is required` }}
-                render={({ field }) => (
-                    <Select onValueChange={field.onChange} value={field.value}>
-                        <SelectTrigger className="w-[300px]">
-                            <SelectValue placeholder={label} />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {
-                                categoryData && categoryData.map(item => (
-                                    <SelectItem key={item.id.toString()} value={item?.id.toString()}>
-                                        {item?.title}
-                                    </SelectItem>
-                                ))
-                            }
-                        </SelectContent>
-                    </Select>
-                )}
-            />
-        </div>
-    )
-}
-
 export default CustomSelect
