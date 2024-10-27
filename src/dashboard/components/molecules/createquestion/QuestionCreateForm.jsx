7eda487832e8887a23bcc84e3e5500ee3d977a94
smansayeed <786saadman@gmail.com>
2024-10-28 00:44:23 +0600

    added delete api, removed required description from que create

--- src/dashboard/components/molecules/createquestion/QuestionCreateForm.jsx
+++ src/dashboard/components/molecules/createquestion/QuestionCreateForm.jsx
@@ -218,7 +218,7 @@ export default function QuestionCreateForm() {
                         <Controller
                             name="description"
                             control={control}
-                            rules={{ required: "Description is required" }}
+                            // rules={{ required: "Description is required" }}
                             render={({ field }) => (
                                 <ReactQuill
                                     theme="snow"
