71a7d1d32ff7f7149a286b856af629e015c67275
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2025-01-09 15:45:06 +0600

    display mcq-option in question table for exma create

--- src/dashboard/components/molecules/modelTests/DataTableForExamCreate.jsx
+++ src/dashboard/components/molecules/modelTests/DataTableForExamCreate.jsx
@@ -1,3 +1,170 @@
+// import {
+//     flexRender,
+//     getCoreRowModel,
+//     getFacetedRowModel,
+//     getFacetedUniqueValues,
+//     getFilteredRowModel,
+//     getPaginationRowModel,
+//     getSortedRowModel,
+//     useReactTable
+// } from "@tanstack/react-table";
+
+// import {
+//     Table,
+//     TableBody,
+//     TableCell,
+//     TableHead,
+//     TableHeader,
+//     TableRow
+// } from "@/components/ui/table";
+
+// import { useState } from "react";
+// import { DataTableToolbar } from "../../organism/datatable/DataTableToolbar";
+// import PaginationForQuesTable from "./PaginationForQuesTable";
+
+// export function DataTableForExamCreate({
+//     columns,
+//     data,
+//     currentPage,
+//     perPage,
+//     onSelectRowIds,
+//     setPerPage,
+//     setCurrentPage,
+//     refetch,
+//     totalRecords
+// }) {
+
+//     const [rowSelection, setRowSelection] = useState({});
+//     const [columnVisibility, setColumnVisibility] = useState({});
+//     const [columnFilters, setColumnFilters] = useState([]);
+//     const [sorting, setSorting] = useState([]);
+
+//     console.log("ROW SELECTION", rowSelection)
+
+//     const table = useReactTable({
+//         data: data || [],
+//         columns: columns || [],
+//         state: {
+//             sorting,
+//             columnVisibility,
+//             rowSelection,
+//             columnFilters
+//         },
+//         enableRowSelection: true,
+//         onRowSelectionChange: (newSelection) => {
+//             setRowSelection(newSelection);
+//             console.log("new selection", newSelection)
+//             const selectedRows = table.getSelectedRowModel().rows;
+//             const selectedIds = selectedRows.map((row) => row.original.id);
+
+//             onSelectRowIds(selectedIds);
+//         },
+//         // onRowSelectionChange: setRowSelection,
+//         onSortingChange: setSorting,
+//         onColumnFiltersChange: setColumnFilters,
+//         onColumnVisibilityChange: setColumnVisibility,
+//         getCoreRowModel: getCoreRowModel(),
+//         getFilteredRowModel: getFilteredRowModel(),
+//         getPaginationRowModel: getPaginationRowModel(),
+//         getSortedRowModel: getSortedRowModel(),
+//         getFacetedRowModel: getFacetedRowModel(),
+//         getFacetedUniqueValues: getFacetedUniqueValues()
+//     });
+
+//     return (
+//         <div className="space-y-4 py-4">
+//             <DataTableToolbar
+//                 table={table}
+//             />
+
+//             <div className="rounded-md border">
+//                 <Table>
+//                     <TableHeader>
+//                         {
+//                             table.getHeaderGroups().map(headerGroup => (
+//                                 <TableRow key={headerGroup.id}>
+//                                     {
+//                                         headerGroup.headers.map(header => {
+//                                             return (
+//                                                 <TableHead key={header.id} colSpan={header.colSpan}>
+//                                                     {header.isPlaceholder
+//                                                         ? null
+//                                                         : flexRender(
+//                                                             header.column.columnDef.header,
+//                                                             header.getContext()
+//                                                         )}
+//                                                 </TableHead>
+//                                             )
+//                                         })
+//                                     }
+//                                 </TableRow>
+//                             ))
+//                         }
+//                     </TableHeader>
+//                     <TableBody>
+//                         {
+//                             table.getRowModel().rows?.length ? (
+//                                 table.getRowModel().rows.map(row => (
+//                                     <TableRow
+//                                         key={row.id}
+//                                         data-state={row.getIsSelected() && "selected"}
+//                                     >
+//                                         {
+//                                             row.getVisibleCells().map(cell => (
+//                                                 <TableCell key={cell.id}>
+//                                                     {flexRender(
+//                                                         cell.column.columnDef.cell,
+//                                                         cell.getContext()
+//                                                     )}
+//                                                 </TableCell>
+//                                             ))
+//                                         }
+//                                     </TableRow>
+//                                 ))
+//                             ) : (
+//                                 <TableRow>
+//                                     <TableCell
+//                                         colSpan={columns.length}
+//                                         className="h-24 text-center"
+//                                     >
+//                                         No results.
+//                                     </TableCell>
+//                                 </TableRow>
+//                             )
+//                         }
+//                     </TableBody>
+//                 </Table>
+//             </div>
+
+//             <PaginationForQuesTable
+//                 table={table}
+//                 data={data}
+//                 currentPage={currentPage}
+//                 perPage={perPage}
+//                 refetch={refetch}
+//                 onPageChange={(newPage) => {
+//                     setCurrentPage(newPage);
+//                     refetch({
+//                         page: newPage,
+//                         perPage: perPage
+//                     });
+//                 }}
+//                 onPerPageChange={(newPageSize) => {
+//                     setPerPage(newPageSize);
+//                     refetch({
+//                         page: currentPage,
+//                         perPage: newPageSize,
+//                     });
+//                 }}
+//                 totalRecords={totalRecords}
+//             />
+//         </div>
+//     )
+// }
+
+
+
+
 import {
     flexRender,
     getCoreRowModel,
@@ -19,31 +186,18 @@ import {
 } from "@/components/ui/table";
 
 import { useState } from "react";
+import { DataTablePagination } from "../../organism/datatable/DataTablePagination";
 import { DataTableToolbar } from "../../organism/datatable/DataTableToolbar";
-import PaginationForQuesTable from "./PaginationForQuesTable";
 
-export function DataTableForExamCreate({
-    columns,
-    data,
-    currentPage,
-    perPage,
-    onSelectRowIds,
-    setPerPage,
-    setCurrentPage,
-    refetch,
-    totalRecords
-}) {
-
+export function DataTableForExamCreate({ columns = [], data = [] }) {
     const [rowSelection, setRowSelection] = useState({});
     const [columnVisibility, setColumnVisibility] = useState({});
     const [columnFilters, setColumnFilters] = useState([]);
     const [sorting, setSorting] = useState([]);
 
-    console.log("ROW SELECTION", rowSelection)
-
     const table = useReactTable({
-        data: data || [],
-        columns: columns || [],
+        data,
+        columns,
         state: {
             sorting,
             columnVisibility,
@@ -51,15 +205,7 @@ export function DataTableForExamCreate({
             columnFilters
         },
         enableRowSelection: true,
-        onRowSelectionChange: (newSelection) => {
-            setRowSelection(newSelection);
-            console.log("new selection", newSelection)
-            const selectedRows = table.getSelectedRowModel().rows;
-            const selectedIds = selectedRows.map((row) => row.original.id);
-
-            onSelectRowIds(selectedIds);
-        },
-        // onRowSelectionChange: setRowSelection,
+        onRowSelectionChange: setRowSelection,
         onSortingChange: setSorting,
         onColumnFiltersChange: setColumnFilters,
         onColumnVisibilityChange: setColumnVisibility,
@@ -72,11 +218,8 @@ export function DataTableForExamCreate({
     });
 
     return (
-        <div className="space-y-4 py-4">
-            <DataTableToolbar
-                table={table}
-            />
-
+        <div className="space-y-4">
+            <DataTableToolbar table={table} />
             <div className="rounded-md border">
                 <Table>
                     <TableHeader>
@@ -136,28 +279,7 @@ export function DataTableForExamCreate({
                 </Table>
             </div>
 
-            <PaginationForQuesTable
-                table={table}
-                data={data}
-                currentPage={currentPage}
-                perPage={perPage}
-                refetch={refetch}
-                onPageChange={(newPage) => {
-                    setCurrentPage(newPage);
-                    refetch({
-                        page: newPage,
-                        perPage: perPage
-                    });
-                }}
-                onPerPageChange={(newPageSize) => {
-                    setPerPage(newPageSize);
-                    refetch({
-                        page: currentPage,
-                        perPage: newPageSize,
-                    });
-                }}
-                totalRecords={totalRecords}
-            />
+            <DataTablePagination table={table} />
         </div>
     )
 }
--- src/dashboard/components/molecules/modelTests/questionsColumns.jsx
+++ src/dashboard/components/molecules/modelTests/questionsColumns.jsx
@@ -1,6 +1,7 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { DataTableColumnHeader } from "@/dashboard/components/organism/datatable/DataTableColumnHeader";
 import { parseHtmlContent } from "@/utils/parseHtmlContent";
+import { Check } from "lucide-react";
 
 export const questionsColumns = [
     {
@@ -27,35 +28,54 @@ export const questionsColumns = [
         enableSorting: false,
         enableHiding: false
     },
-    // {
-    //     accessorKey: "id",
-    //     header: ({ column }) => (
-    //         <DataTableColumnHeader column={column} title="Serial No" />
-    //     ),
-    //     cell: ({ row }) => {
-    //         console.log("row", row)
-    //         const index = row.index + 1;
-    //         return (
-    //             <div className="w-[80px]">{index}</div>
-    //         )
-    //     },
-    //     enableSorting: false,
-    //     enableHiding: false
-    // },
+    {
+        accessorKey: "id",
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title="Serial No" />
+        ),
+        cell: ({ row }) => {
+            const index = row.index + 1;
+            return (
+                <div className="w-[80px]">{index}</div>
+            )
+        },
+        enableSorting: false,
+        enableHiding: false
+    },
     {
         accessorKey: "title",
         header: ({ column }) => (
             <DataTableColumnHeader column={column} title="Title" />
         ),
         cell: ({ row }) => {
+            const mcqOptions = row.original.mcq_questions || [];
+            console.log("mcqoptions", mcqOptions)
 
             return (
-                <div className="flex space-x-2">
-                    <span className="max-w-32 truncate font-medium sm:max-w-72 md:max-w-[31rem]">
+                <div className="flex flex-col space-y-2">
+                    <span className="font-medium">
                         {parseHtmlContent(row.getValue("title"))}
                     </span>
+                    <div className="grid grid-cols-2 gap-2">
+                        {mcqOptions.map((option) => (
+                            <div
+                                key={option.id}
+                                className="flex items-center space-x-1 p-2 rounded-md border-gray-400"
+                            >
+                                <span className={`text-sm font-medium min-w-[20px] ${option.is_correct && 'text-green-500'}`}>
+                                    {option.mcq_option_serial}.
+                                </span>
+                                <span className={`text-sm ${option.is_correct && 'text-green-500'}`}>
+                                    {parseHtmlContent(option.mcq_question_text)}
+                                </span>
+                                {
+                                    option?.is_correct && <Check className="text-green-600" />
+                                }
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )
         }
-    }
+    },
 ]
